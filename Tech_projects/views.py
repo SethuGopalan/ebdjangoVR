e19a515aad9bfd1e47506f9f21e9039b577b7c4c
Sethu Gopalan <Sethu.Gopalan@cytel.com>
2021-06-18 16:19:09 -0400

    Created Tech_project_Index in the view.py

--- Tech_projects/views.py
+++ Tech_projects/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from Tech_projects.models import Tech_project
+
+def Tech_project_index(request):
+    Tech_projects = Tech_project.objects.all()
+    context = {
+        'Tech_projects' : Tech_projects
+    }
+    return render(request, 'Tech_project_index.html',context)
+     
