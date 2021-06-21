9ff7d32ebe0c6819efb9bcfdb9a079ca5e2dbd29
Sethu Gopalan <Sethu.Gopalan@cytel.com>
2021-06-21 14:50:01 -0400

    templates Tech_project_index and tech_project_details  added

--- Tech_projects/urls.py
+++ Tech_projects/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.Tech_project_index,name="Tech_project_index"),
-    path("<int:key>/",views.Tech_project_details,name="Tech_project_details"),
+    path("", views.Tech_project_index, name="Tech_project_index"),
+    path("<int:key>/", views.Tech_project_details, name="Tech_project_details"),
 ]
--- ebdjangoVR/settings.py
+++ ebdjangoVR/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')pwq@-na8^#yofdsvjte9_5+9l2+%8fwe-wdmw3@nxpel8bv+4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'ebdjangoVR.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["Tech_projects/ templates/"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,5 +117,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
